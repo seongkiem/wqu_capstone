2ac2b3babc42263f957f2bc76e693501b16fbc1d
seongkiem <54034028+seongkiem@users.noreply.github.com>
2021-01-23 21:20:32 +0800

    Update EnvMultipleStock_validation.py

--- env/EnvMultipleStock_validation.py
+++ env/EnvMultipleStock_validation.py
@@ -139,8 +139,10 @@ class StockEnvValidation(gym.Env):
 
             df_total_value.columns = ['account_value']
             df_total_value['daily_return']=df_total_value.pct_change(1)
+            # daily risk free rate.
+            rf = (1.02147848084291189**(1/252))-1 
             if df_total_value['daily_return'].std()!=0:
-                sharpe = (4**0.5)*df_total_value['daily_return'].mean()/ \
+                sharpe = (4**0.5)*(df_total_value['daily_return'].mean()-rf)/ \
                   df_total_value['daily_return'].std()
             else:
                 sharpe=0
